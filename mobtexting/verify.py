9bd11b35f61df8d8883d56f2e01d2039ab24d3f6
vishal8282 <vishalnishad888@gmail.com>
2019-02-11 12:59:12 +0530

    verify add cancel and params for send

--- mobtexting/verify.py
+++ mobtexting/verify.py
@@ -9,12 +9,17 @@ class Verify:
             'Authorization': access_token
         }
 
-    def send(self, to):
+    def send(self, to, data={}):
         url = api_endpoint + 'verify?to=' + to 
-        r = requests.post(url, headers=self.headers)
+        r = requests.post(url, headers=self.headers, data=data)
         return r
 
     def check(self, id, token):
         url = api_endpoint + 'verify/check/' + id + '/' + token
+        r = requests.get(url, headers=self.headers)
+        return r
+
+    def cancel(self, id):
+        url = api_endpoint + 'verify/cancel/' + id
         r = requests.get(url, headers=self.headers)
         return r
